7b90792cd3e357b57e49b005b07d94a61a0f568f
Tord <tord.fauskanger@gmail.com>
2010-03-30 20:42:41 +0200

    first commit

---
+++ ProjectDirectory4/src/org/lejos/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.lejos.example;
+
+import lejos.nxt.*;
+
+/**
+ * Example leJOS Project with an ant build file
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+                a
+		Button.waitForPress();
+	}
+}
